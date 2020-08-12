fe60f17bd2afc39f5c0e28758718e49d8936af89
Chuck Lu <chuck.lu@qq.com>
2020-08-12 17:29:21 +0800

    add test code https://cachemanager.michaco.net/documentation/CacheManagerGettingStarted

--- CacheManagerTest/Program.cs
+++ CacheManagerTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using CacheManager.Core;
 using Microsoft.Extensions.Configuration;
+using ConfigurationBuilder = Microsoft.Extensions.Configuration.ConfigurationBuilder;
 
 namespace CacheManagerTest
 {
@@ -13,6 +15,20 @@ namespace CacheManagerTest
                 var configurationRoot = builder.Build();
                 var cacheConfiguration = configurationRoot.GetCacheConfiguration();
                 Console.WriteLine($"MaxRetries = {cacheConfiguration.MaxRetries}");
+
+                var cache = CacheFactory.FromConfiguration<object>(cacheConfiguration);
+                cache.Add("keyA", "valueA");
+                cache.Put("keyB", 23);
+                cache.Update("keyB", v => 42);
+
+                Console.WriteLine("KeyA is " + cache.Get("keyA"));      // should be valueA
+                Console.WriteLine("KeyB is " + cache.Get("keyB"));      // should be 42
+                cache.Remove("keyA");
+
+                Console.WriteLine("KeyA removed? " + (cache.Get("keyA") == null).ToString());
+
+                Console.WriteLine("We are done...");
+
             }
             catch (Exception ex)
             {
